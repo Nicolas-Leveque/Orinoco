1cb1bffde1fffbf8b68030d662730db052bf62d1
Nicolas Lévêque <nicoleveque@outlook.com>
2020-09-28 02:25:23 +0200

    refactorisation basket.js, début de travail dur la requête POST

--- js/basket.js
+++ js/basket.js
@@ -1,6 +1,8 @@
 let panierArray = [];
 let basket = [];
-
+let productDetails;
+let contact;
+let product_id = [];
 function retrieveLocalStorage() {
     //extrait les infos des produits dans le panier et les stock dans un array d'objet
     return new Promise((resolve, reject) => {
@@ -10,52 +12,44 @@ function retrieveLocalStorage() {
             }else {
                 return
             };
-        };
-        if (panierArray != []) {
-            console.log(panierArray, "fct1")
-            resolve(panierArray);
-        }
-        else {
-            reject(Error("pas bon"));
-        }
+        };        
+        resolve(panierArray);
+        });   
+};
+function getProductsDetails() {
+    //envoie une requête pour récupérer les infos produits
+    return new Promise((resolve, reject) => {                
+        let request = new XMLHttpRequest();
+        request.open("GET", `http://localhost:3000/api/cameras/`);
+        request.send();
+        request.onreadystatechange = function () {
+        if (this.readyState == XMLHttpRequest.DONE && this.status)
+            { 
+                productDetails = JSON.parse(this.responseText);
+                resolve(productDetails)
+                };                
+            };        
+            
     });
 };
-function createBasket(id) {
-    console.log('FCT 1.5')
-    //pour chaque élément de l'array envoie une requête vers le serveur pour récupérer les données manquante et créer un array d'objet
+function createBasket() {
+    //créer un array d'objets contenant les infos nécessaire en combinant les infos tirées du localStorage et celles de l'API
     return new Promise((resolve, reject) => {
-
-        id.forEach((product) => {        
-            let request = new XMLHttpRequest();
-            request.open("GET", `http://localhost:3000/api/cameras/${product.id}`);
-            request.send();
-            request.onreadystatechange = function () {
-                if (this.readyState == XMLHttpRequest.DONE && this.status)
-                    {                
-                        let response = JSON.parse(this.responseText);
-                        let tmpObj = {id: response._id, name: response.name, lense: product.lense, quantity: product.quantity, price: response.price/100};
-                        basket.push(tmpObj);
-                        console.log("fct2");
-                        console.table(basket);
-                        checkStorageTables(panierArray, basket, resolve);
-                    };                
-            };        
-        })
-        
-        if (basket != []) {
-            console.log("fct2.5");
-            //resolve(basket)
-        } 
-        else {
-            reject(Error("raté"));
-        }
+        panierArray.forEach((objectLocal) => {
+            productDetails.forEach((objectProduct) => {
+                if(objectLocal.id === objectProduct._id) {
+                    let tmpObj = {};
+                    tmpObj = {id: objectProduct._id, name: objectProduct.name, lense: objectLocal.lense, quantity: objectLocal.quantity, price: objectProduct.price/100};
+                    basket.push(tmpObj);
+                    checkStorageTables(panierArray, basket, resolve);
+                }
+            });
+        });
     });
 };
 
 function createHTMLTable (){
-    console.log("fct3")    
-    console.table(basket);
-    
+    //Affiche le panier sur la page HTML
     const infoPanier = document.getElementById("info-panier");
     infoPanier.innerHTML = '';
     basket.forEach((product) => {
@@ -70,16 +64,15 @@ function createHTMLTable (){
             infoPanier.appendChild(newRow);
     });   
 };
-function checkStorageTables(panierBrut, panierFinal, resolve) {
-    console.log('debut')
-    if(panierFinal.length === panierBrut.length) {
-        console.log('resolue')
-        resolve(panierFinal)
+function checkStorageTables(panierArray, basket, resolve) {
+    if(basket.length === panierArray.length) {
+        resolve(basket)
     }
 }
 
 retrieveLocalStorage()
-    .then(createBasket(panierArray)
+    .then(getProductsDetails()
+    .then(createBasket)
     .then(createHTMLTable))
 
 //bouton vider le panier
@@ -91,11 +84,71 @@ document.getElementById("vide-panier").addEventListener("click", function (e) {
     window.location.reload();
   });
 
-//récupération des données du formulaire
-const prenomElt =  document.getElementById('prenom');
-const nomElt =  document.getElementById('nom');
-const adresseElt =  document.getElementById('adresse');
-const villeElt =  document.getElementById('ville');
-const emailElt =  document.getElementById('email');
+function contactInfo() {
+    //récupération des données du formulaire et création d'un objet de contact
+    const prenomElt =  document.getElementById('prenom');
+    const nomElt =  document.getElementById('nom');
+    const adresseElt =  document.getElementById('adresse');
+    const villeElt =  document.getElementById('ville');
+    const emailElt =  document.getElementById('email');
+    
+    if (!/[a-z]/i.test(prenomElt.value)) {
+        return
+        } else {
+            if (!/[a-z]/i.test(nomElt.value)) {
+                return
+            } else {
+                if (!/[a-z]/i.test(adresseElt.value)) {
+                    return
+                } else {
+                    if (!/[a-z]/i.test(villeElt.value)) {
+                        return
+                    } else {
+                        if (!/[a-z]/i.test(emailElt.value)) {
+                            return
+                            } else {
+                                contact = {
+                                    firstName: prenomElt.value,
+                                    lastName: nomElt.value,
+                                    adress: adresseElt.value,
+                                    city: villeElt.value,
+                                    email: emailElt.value
+                                }
+                            }
+                        }
+                    }           
+                }
+            } 
+}
+function createBasketArray() {
+    product_id =[];
+    basket.forEach((product) => {
+        product_id.push(product.id)
+    })
+}
+function checkoutRequest() {
+    let request = new XMLHttpRequest;
+    request.open("POST", "http://localhost:3000/api/cameras/order")
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(JSON.stringify(contact), JSON.stringify(product_id));
+    request.onreadystatechange = function() {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            let response = this.responseText;
+            console.log(response);
+        }
+    }
+    
+}
+
+document.getElementById("confirm").addEventListener("click", (e) => {
+    e.preventDefault();
+    contactInfo();
+    createBasketArray();
+    console.log(contact);
+    console.log(product_id);
+    checkoutRequest();
+    
+    
+});
 
 
