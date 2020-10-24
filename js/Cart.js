4d42d07d2b07fd914af09cb8b75183cf59a3eddf
Nicolas Lévêque <nicoleveque@outlook.com>
2020-10-25 00:25:44 +0200

    Légère modif Cat.js et ajustement respopnsive

--- js/Cart.js
+++ js/Cart.js
@@ -3,14 +3,14 @@ let Cart = {
     init() {
         //extrait les infos des produits dans le panier et les stock dans un array d'objet
         return new Promise((resolve, reject) => {
-            if (Cart.items.length != localStorage.length){
+            
                 Cart.items = [];
                 for (let i = 0; i <= localStorage.length - 1 ; i++) {
                     if (localStorage.key(i).startsWith("5be")) {
                         Cart.items.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
                     }
                 };
-            };
+            
             resolve(Cart.items);
         });   
     }
